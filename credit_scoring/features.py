b88fdae52ec333b13f2285b5538544f054e89503
isailzc <isai.vazquezlzc@gmail.com>
2026-09-15 12:42:06 -0600

    complete data quality notebook and update environment dependencies

--- credit_scoring/features.py
+++ credit_scoring/features.py
@@ -4,7 +4,7 @@ from loguru import logger
 from tqdm import tqdm
 import typer
 
-from credit-scoring.config import PROCESSED_DATA_DIR
+from credit_scoring.config import PROCESSED_DATA_DIR
 
 app = typer.Typer()
 
